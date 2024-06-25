4cccf48849f315e1e8d3dfed3fdeb2c92c39fdb5
TianMeds <cbmedallada@student.apc.edu.ph>
2024-06-25 09:38:34 +0800

    Change and fix the Login and Register UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,52 +4,82 @@ const Login = () => {
   return (
 <>
     <div className="bg-cover bg-center h-screen flex justify-center items-center" style={{ backgroundImage: "url('https://raw.githubusercontent.com/TianMeds/image--stocks-for-coding/fe1eba25fc9bbacf2a5df93931e08d2f331b68ca/GR%20Eatery.svg')" }}>
-        <div className="w-6/12 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-            <form className="space-y-6" action="#">
-                <h5 className="text-2xl font-bold text-gray-900 text-center">Sign in to G & R Eatery </h5>
-                <div className="text-sm font-medium text-gray-600 flex justify-center">
-                    <span className='mr-1'>New User?</span> <a href="/register" className="text-black-700 hover:underline dark:text-blue-500"> Create account</a>
-                </div>
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email Address</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email address" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
-                </div>
-                <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Enter your password" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
-                </div>
-                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-                <div className="flex items-start">
-                    <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"/>
-                        </div>
-                        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900">Remember me</label>
-                    </div>
-                    <a href="#" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot Password?</a>
-                </div>
-                <div className="flex items-center justify-center my-4">
-                    <div className="border-b border-gray-400 w-1/4"></div>
-                    <span className="mx-2 text-gray-600 text-sm">Or login with</span>
-                    <div className="border-b border-gray-400 w-1/4"></div>
-                </div>
-                <div className="flex justify-center">
-                    <button className="bg-red-500 text-white px-4 py-2 rounded-md mr-2 flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30" fill='white'>
-                            <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path>
-                        </svg>
-                        <span className="ml-2 text-sm">Google</span>
-                    </button>
-
-                    <button className="bg-blue-700 text-white px-4 py-2 rounded-md flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30" fill="white">
-                            <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"></path>
-                        </svg>
-                        <span className="ml-2 text-sm">Facebook</span>
-                    </button>
-                </div>
-            </form>
+      <div className="bg-white shadow-lg rounded-lg flex w-full max-w-4xl p-8">
+        {/* Left Side */}
+        <div className="w-1/2 flex flex-col">
+          <div className="flex flex-col items-center mb-4 mt-8">
+            <img src="https://raw.githubusercontent.com/TianMeds/image--stocks-for-coding/274a634a12930720116a85d65cf5930559da76bf/G%26R%20Eatery%20Logo.svg" alt="Logo" className="h-40" /> 
+          </div>
+          <h2 className="text-2xl font-bold text-gray-800 text-center mb-4">Login to your account</h2>
+          <p className="text-gray-600 text-center">
+            Please login to access your cart and book an order
+          </p>
         </div>
+
+        {/* Right Side */}
+        <div className="w-1/2 flex flex-col justify-center p-8 rounded-r-lg">
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="flex items-center justify-center mb-4">
+              <button
+                type="submit"
+                className=" w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Sign in to account
+              </button>
+            </div>
+            <a
+                href="#"
+                className="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-blue-800"
+              >
+                Forgot your password?
+              </a>
+          </form>
+          <div className="flex items-center justify-center my-4">
+            <span className="border-b border-gray-400 w-full"></span>
+            <span className="text-gray-500 mx-4">or</span>
+            <span className="border-b border-gray-400 w-full"></span>
+          </div>
+          <div className="flex flex-col items-center">
+          <button
+            type="button"
+            className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center mb-4 w-full"
+            >
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
+                alt="Google Logo"
+                className="h-5 w-5 mr-2"
+            />
+            Sign up with Google
+          </button>
+            <p className="text-gray-600">
+              Don't have an account?{' '}
+              <a href="/register" className="text-blue-500 hover:text-blue-800">
+                Sign up now
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
 </>
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,51 +19,63 @@ const Register = () => {
   return (
     <>
       <div className="bg-cover bg-center h-screen flex justify-center items-center" style={{ backgroundImage: "url('https://raw.githubusercontent.com/TianMeds/image--stocks-for-coding/fe1eba25fc9bbacf2a5df93931e08d2f331b68ca/GR%20Eatery.svg')" }}>
-        <div className="w-6/12 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-          <form className="space-y-6" action="#">
-            <h5 className="text-xl font-bold text-gray-900 text-center">Be a G & R Eatery Member</h5>
-            <div className="text-sm font-medium text-gray-500 flex justify-center">
-              <span className="mr-1"> Already Registered? </span><a href="/login" className="text-blue-700 hover:underline dark:text-blue-500">Login to your account</a>
+        <div className="w-6/12 md:w-8/12 lg:w-9/12 p-6 bg-white border border-gray-200 rounded-lg shadow sm:p-8 flex flex-col md:flex-row">
+          {/* Form Section */}
+          <div className="md:w-1/2 p-4">
+            <h5 className="text-xl font-bold text-gray-900 text-center mb-4">Be a G & R Eatery Member</h5>
+            <div className="text-sm font-medium text-gray-500 flex justify-center mb-4">
+              <span className="mr-1">Already Registered?</span>
+              <a href="/login" className="text-blue-700 hover:underline dark:text-blue-500">Login to your account</a>
             </div>
-            <div>
-              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Full Name</label>
-              <input type="name" name="name" id="name" placeholder="Enter your full name" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
-            </div>
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-              <input type="email" name="email" id="email" placeholder="Enter your email address" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
-            </div>
-            <div className="flex flex-col md:flex-row md:space-x-2">
-              <div className="flex-grow">
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                <input type="password" name="password" id="password" placeholder="Enter your password" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
+            <form className="space-y-4" action="#">
+              <div>
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Full Name</label>
+                <input type="text" name="name" id="name" placeholder="Enter your full name" className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
               </div>
-              <div className="flex-grow">
-                <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
-                <input type="password" name="confirmpwd" id="confirmpwd" placeholder="Confirm your password" className="w-full border-b border-gray-400 focus:outline-none focus:border-blue-500 text-sm" />
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                <input type="email" name="email" id="email" placeholder="Enter your email address" className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
               </div>
-            </div>
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                <input type="password" name="password" id="password" placeholder="Enter your password" className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
               </div>
-              <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900">
-                By signing up you agree to our{' '}
-                <button onClick={toggleModal} className="text-blue-700 hover:underline dark:text-blue-500 focus:outline-none">Terms and Conditions</button>{' '}
-                and{' '}
-                <button onClick={togglePrivacy} className="text-blue-700 hover:underline dark:text-blue-500 focus:outline-none">Privacy Policy</button>{' '}
-              </label>
-            </div>
-            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create account</button>
-          </form>
+              <div>
+                <label htmlFor="confirmpwd" className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
+                <input type="password" name="confirmpwd" id="confirmpwd" placeholder="Confirm your password" className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
+              </div>
+              <div className="flex items-start mb-4">
+                <div className="flex items-center h-5">
+                  <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" />
+                </div>
+                <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">
+                  By signing up you agree to our{' '}
+                  <button type="button" onClick={toggleModal} className="text-blue-700 hover:underline dark:text-blue-500 focus:outline-none">Terms and Conditions</button>{' '}
+                  and{' '}
+                  <button type="button" onClick={togglePrivacy} className="text-blue-700 hover:underline dark:text-blue-500 focus:outline-none">Privacy Policy</button>{' '}
+                </label>
+              </div>
+              <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create account</button>
+            </form>
+          </div>
+          {/* Text Section */}
+          <div className="w-1/2 flex flex-col">
+          <div className="flex flex-col items-center mb-4 mt-8">
+            <img src="https://raw.githubusercontent.com/TianMeds/image--stocks-for-coding/274a634a12930720116a85d65cf5930559da76bf/G%26R%20Eatery%20Logo.svg" alt="Logo" className="h-40" /> 
+          </div>
+          <h2 className="text-2xl font-bold text-gray-800 text-center mb-4">Be a G & R Eatery Member</h2>
+          <p className="text-gray-600 text-center mx-7">
+            Please register for you to book an order and eliminate the hassle of waiting in line
+          </p>
+        </div>
         </div>
       </div>
 
-      {/* Term & Condition Modal */}
+      {/* Terms and Conditions Modal */}
       {termModal && (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg">
-            <span className="absolute top-0 right-0 cursor-pointer" onClick={toggleModal}>&times;</span>
+          <div className="bg-white p-6 rounded-lg relative">
+            <span className="absolute top-0 right-0 cursor-pointer text-gray-500 hover:text-gray-700" onClick={toggleModal}>&times;</span>
             <div>
               <h2 className="text-lg font-semibold mb-2">Terms and Conditions</h2>
               <p className="text-sm">Your terms and conditions content goes here...</p>
@@ -73,15 +85,14 @@ const Register = () => {
         </div>
       )}
 
-      
-      {/* Term & Condition Modal */}
+      {/* Privacy Policy Modal */}
       {privacyModal && (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg">
-            <span className="absolute top-0 right-0 cursor-pointer" onClick={toggleModal}>&times;</span>
+          <div className="bg-white p-6 rounded-lg relative">
+            <span className="absolute top-0 right-0 cursor-pointer text-gray-500 hover:text-gray-700" onClick={togglePrivacy}>&times;</span>
             <div>
               <h2 className="text-lg font-semibold mb-2">Privacy Policy</h2>
-              <p className="text-sm">Your terms and conditions content goes here...</p>
+              <p className="text-sm">Your privacy policy content goes here...</p>
               <button onClick={togglePrivacy} className="mt-4 bg-blue-700 text-white px-4 py-2 rounded-md">Close</button>
             </div>
           </div>
